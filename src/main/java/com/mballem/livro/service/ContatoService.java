4717b534379ed7e690aa31f21c2884d4f445ce73
Ballem <romarcio@gmail.com>
2019-05-20 14:53:57 -0300

    Inclusão das anotações de transações do spring.

--- src/main/java/com/mballem/livro/service/ContatoService.java
+++ src/main/java/com/mballem/livro/service/ContatoService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
@@ -21,11 +22,13 @@ public class ContatoService {
 	@Autowired
 	private ContatoRepository repository;
 
+	@Transactional
 	public void salvar(Contato contato) {
 		contato.setDtCadastro(new Date());
 		repository.save(contato);
 	}
 
+	@Transactional
 	public void alterar(Contato contato) {
 		Contato c = repository.findOne(contato.getId());
 		c.setIdade(contato.getIdade());
@@ -42,24 +45,29 @@ public class ContatoService {
 		repository.save(c);
 	}
 
+	@Transactional
 	public void excluir(Long id) {
 		repository.delete(id);
 	}
 
+	@Transactional(readOnly = true)
 	public Contato buscaPorId(Long id) {
 		return repository.findOne(id);
 	}
 
+	@Transactional(readOnly = true)
 	public Page<Contato> paginar(int start, int size, Sort sort) {
 		Pageable pageable = new PageRequest(start , size, sort);
 		return repository.findAll(pageable);
 	}
 
+	@Transactional(readOnly = true)
 	public Page<Contato> paginar(int start, int size, Sort sort, String sFilter) {
 		Pageable pageable = new PageRequest(start, size, sort);
 		return repository.findByFiltros(sFilter, pageable);
 	}
 
+	@Transactional(readOnly = true)
 	public Page<Contato> filterByIdade(int start, int size, Sort sort, Integer idade) {
 		Pageable pageable = new PageRequest(start, size, sort);
 		return repository.findByIdade(idade, pageable);
